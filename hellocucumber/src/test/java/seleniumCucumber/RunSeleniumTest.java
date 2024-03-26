7b3c9ea6cd7b9b961a05c19386f8ecb032125631
Melad <melad.alamri@gmail.com>
2024-03-26 13:25:31 +0300

    add filter by tags and the report

---
+++ hellocucumber/src/test/java/seleniumCucumber/RunSeleniumTest.java
@@ -0,0 +1,16 @@
+package seleniumCucumber;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.*;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectPackages("seleniumCucumber")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber.html")
+@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@Search")
+public class RunSeleniumTest {
+}
